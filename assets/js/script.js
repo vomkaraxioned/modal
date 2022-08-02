1ac66e8946ad0526c9fe05a6a2d8ff3124ff9d26
vomkaraxioned <omkarv@axioned.com>
2022-08-02 14:41:56 +0530

    csss changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,6 @@
-/* Author: 
-
-*/
-let img = document.getElementById("mybtn");
-let li = document.getElementsByClassName("fig");
-img.addEventListener("click", displayDate);
-function displayDate () {
-  document.getElementById("demo").innerHTML = li.length;
-}
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+    $("li").click(function() {
+        $(".fig").removeClass("active");
+        $(this).addClass("active");
+    });
+});
